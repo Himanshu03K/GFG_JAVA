7e8ae438ba993b73a12915f021bbabc173d1b496
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-17 23:01:49 +0530

    One's Complement

---
+++ One's Complement .java
@@ -0,0 +1,35 @@
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.onesComplement(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int onesComplement(int N){
+        //code here
+        int n=N,i=0;
+        int ans=0;
+        while(N>0){
+            ans += (1<<i); 
+            i++;
+            N=N>>1;
+        }
+        return ans^n;
+    }
+}
