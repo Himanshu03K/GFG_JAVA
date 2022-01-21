6e4255cb8ed887f3f9a0a6dc961cc8eeffcbf154
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-21 23:09:22 +0530

    Smallest Positive missing number

---
+++ Smallest Positive missing number.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        Arrays.sort(arr);
+        int c=1;
+        for(int i=0;i<size;i++){
+            if(arr[i]==c)
+            c++;
+        }
+        return c;
+    }
+}
+
+class Main
+{   
+    public static void main (String[] args) 
+    {
+
+		Scanner sc=new Scanner(System.in);
+		
+        //taking testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //input number n
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=sc.nextInt();
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			System.out.println(missing);
+		}
+    }
+}
