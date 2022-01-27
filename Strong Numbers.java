dae8daa3c390f6249bfcaa29fc558ba3a425a903
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-27 22:54:22 +0530

    Strong Numbers

--- Strong Numbers.java
+++ Strong Numbers.java
@@ -1,1 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.is_StrongNumber(n);
+            System.out.println(ans);
+        }
+    }
+}
 
+
+class Solution
+{
+    static int fact(int x){
+        if(x==0 || x==1)
+        return 1;
+        
+        return x*fact(x-1);
+    }
+    public int is_StrongNumber(int n)
+    {
+        // Code here
+        int x=n,sum=0;
+        while(n>0){
+            sum+=fact(n%10);
+            n/=10;
+        }
+        if(x==sum)
+        return 1;
+        return 0;
+    }
+}
