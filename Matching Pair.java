2bcbcde902f08c48083f467c757aa9675c87aaa1
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-01-29 23:38:13 +0530

    Matching Pair

--- Matching Pair.java
+++ Matching Pair.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
 
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.find(N));
+        }
+    }
+}
+
+class Solution{
+    static long find(int N){
+        // code here
+        return N+1;
+    }
+}
