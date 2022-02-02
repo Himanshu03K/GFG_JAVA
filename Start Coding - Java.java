2baf2166cfa86782d51aaba5ce9bcc451eee7c61
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-02 23:37:21 +0530

    Start Coding - Java

--- Start Coding - Java.java
+++ Start Coding - Java.java
@@ -1,1 +1,24 @@
+class Geeks{
+    
+    // Function to print hello
+    static void printHello(){
+        
+        // Your code here
+        System.out.println("Hello World");
+    }
+    
+}
 
+
+class GfG{
+    
+    public static void main(String args[]){
+       
+       //Creating an Object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling printHello() function of the Class Geeks
+        g.printHello();
+    }
+    
+}  
