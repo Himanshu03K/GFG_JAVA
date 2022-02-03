b06d03fff7608637eafcc8fee494a74dbe93c5ad
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-03 23:22:09 +0530

    Equilibrium Point

--- Equilibrium Point.java
+++ Equilibrium Point.java
@@ -1,1 +1,57 @@
 
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        int sum =0;
+        int sum2 = 0;
+        
+        for(int i = 0; i<n; i++){
+            sum+= arr[i];
+        }
+        for(int j = 0; j < n; j++){
+            if(sum2 == sum - arr[j]){
+                return j + 1;
+            }
+            sum-=arr[j];
+            sum2+= arr[j];
+        }
+        return -1;
+    }
+}
