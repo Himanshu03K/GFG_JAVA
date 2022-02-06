9c65628874569ce3271dab2afe25d94e0b5fc524
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-06 23:51:20 +0530

    Count the Zeros

--- Count the Zeros.java
+++ Count the Zeros.java
@@ -1,1 +1,44 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().countZeroes(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+class Solution {
+    int countZeroes(int[] arr, int n) {
+        // code here
+        int i=0,c=0;
+        n=n-1;
+        while(i<=n){
+            if(i==n){
+                if(arr[i++]==0)
+                c++;
+                break;
+            }
+            if(arr[i++]==0)
+            c++;
+            if(arr[n--]==0)
+            c++;
+        }
+        return c;
+    }
+
+}
