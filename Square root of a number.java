1aa7c2b024f87eab17c180dcfa8bd002343e7153
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-10 23:55:50 +0530

    Square root of a number

--- Square root of a number.java
+++ Square root of a number.java
@@ -13,14 +13,7 @@ class SquartRoot
 		t--;
 		}
 	}
-}// } Driver Code Ends
-
-
-/*You are required to complete
-this function*/
-
-// Function to find square root
-// x: element to find square root
+}
 class Solution
 {
      long floorSqrt(long x)
