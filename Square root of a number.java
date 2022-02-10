88a3514ededb65e04aa76e4f75020f3a3b8354b6
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-10 23:55:01 +0530

    Square root of a number

--- Square root of a number.java
+++ Square root of a number.java
@@ -1,1 +1,45 @@
 
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		int c=0;
+        for(int i=1; i<=x; i++)
+        {
+            if(i*i==x)
+            {
+                c=i;
+                break;
+            }
+            else if( i*i>x)
+            {
+                c=i-1;
+                break;
+            }
+        }
+        return c;
+	 }
+}
