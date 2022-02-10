c9e43f4b415ced289157acaa80f0d65478c2d1df
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-10 23:57:20 +0530

    Nearly Sorted Algorithm

--- Nearly Sorted Algorithm.java
+++ Nearly Sorted Algorithm.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc=new Scanner(System.in);
+     int t=sc.nextInt();
+     while (t-->0)
+     {
+         int n=sc.nextInt();
+         int k=sc.nextInt();
+         int arr[]=new int[n];
+         for(int i=0;i<n;i++)
+         arr[i]=sc.nextInt();
+         Arrays.sort(arr);
+         for(int i:arr)
+         System.out.print(i+" ");
+         System.out.println();
+	 }
+	}
+}
