4ac0b03750362741c6db9a1c17215b278714459e
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-12 23:40:44 +0530

    The dice problem

--- The dice problem.java
+++ The dice problem.java
@@ -1,1 +1,25 @@
+import java.io.*;
+import java.util.*;
 
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            
+            Solution ob = new Solution();
+            int ans  = ob.oppositeFaceOfDice(N);
+            System.out.println(ans);
+        }
+    }
+}
+class Solution{
+    static int oppositeFaceOfDice(int N){
+        return 7-N;
+    }
+}
