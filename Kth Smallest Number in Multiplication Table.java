c30f1cf1c717baf5314c160b59f5d1da5ad8d1ef
HIMANSHU KUSHWAHA <82281146+Himanshu03K@users.noreply.github.com>
2022-02-12 22:44:12 +0530

    Kth Smallest Number in Multiplication Table

--- Kth Smallest Number in Multiplication Table.java
+++ Kth Smallest Number in Multiplication Table.java
@@ -1,1 +1,43 @@
+import java.io.*;
+import java.util.*;
 
+
+class Array {
+	public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            
+            System.out.println(ob.KthSmallest(m,n, k));
+        }
+        
+	}
+}
+
+class Solution {
+    public int KthSmallest(int m, int n, int k) {
+        int low=1,high=m*n;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(smaller(mid,m,n,k))
+                high=mid;
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+    
+    public boolean smaller(int num,int m,int n,int k){
+        int count=0;
+        for(int i=1;i<=m;i++)
+            count+=Math.min(num/i,n);
+        return count>=k;
+    }
+}
